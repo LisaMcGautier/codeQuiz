1ef0a3051bd93e63e7d33235a590da38c3d56fa4
Lisa McDonald <nixxxxer@aol.com>
2020-07-18 22:25:25 -0400

    Basic Layout

---
+++ script.js
@@ -0,0 +1,23 @@
+// scores internal link (anchor)
+
+// timer at 0 when page loads && starts onclick start button
+// function in previous activity
+
+// title h1 && array [] of questions getElementById.innerHTML "question"
+
+// objective p && possible answers
+// array [] of objects {3 incorrect answers && 1 correct answer}
+// incorrect answers decrement timer
+// all answers activate next question
+// function
+// loop
+
+// start button
+// disappears onclick
+
+// GAME ENDS WHEN TIMER = 0 || ALL QUESTIONS ANSWERED
+
+
+
+// an idea: background color flashes green (1 sec?) if correct
+// flashes red if incorrect
